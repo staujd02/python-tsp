3123347259c73513730c4b376278bf21771f8c93
Joel <staujd02@students.ipfw.edu>
2020-03-01 22:08:59 -0500

    in the middle of changing graphs from list holders to key holders

--- source/tests/graph_test.py
+++ source/tests/graph_test.py
@@ -27,104 +27,105 @@ class Graph_test(unittest.TestCase):
         "B->A": Vector('B', 'A', 0),
         "C->E": Vector('C', 'E', 0),
         "D->A": Vector('D', 'A', 0),
+        "E->A": Vector('E', 'A', 0),
     }
     
     def test_a_graph_can_copy_itself(self):
         V = self.V
-        graph = Graph([V['D->E'], V['E->B'], V['B->D'], V['C->B']])
+        graph = Graph(['A->B', 'B->D', 'C->B', 'D->A'], V)
         self.assertTrue(graph is not graph.copy())
         self.assertEqual(str(graph), str(graph.copy()))
         self.assertEqual(graph.lastChange, graph.copy().lastChange)
-        self.assertEqual(graph.graphLength, graph.copy().graphLength)
+    
+    def test_a_graph_can_translate_an_ascii_key_to_an_index(self):
+        V = self.V
+        graph = Graph(['A->B', 'B->D', 'C->B', 'D->A'], V)
+        self.assertEqual(graph.translate('A'), 0)
+        self.assertEqual(graph.translate('C'), 2)
     
     def test_a_graph_can_go_deeper(self):
         V = self.V
-        graph = Graph([V['D->E'], V['E->B'], V['B->D'], V['C->B']])
+        graph = Graph(['A->B', 'B->D', 'C->B', 'D->A'], V)
         w = graph.getWeight()
         newWeight = graph.goDeeper(V['E->C'], w)
         self.assertEqual(newWeight, w + V['E->C'][2])
-        self.assertEqual(graph.data['E'], V['E->C'])
-        self.assertEqual(graph.lastChange, V['E->B'])
+        self.assertEqual(graph.data[4], 'E->C')
+        self.assertEqual(graph.lastChange, 'E->B')
     
     def test_a_graph_can_go_across(self):
         V = self.V
-        vList = [V['D->E'], V['E->B'], V['B->D'], V['C->B']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->D', 'C->B', 'D->A', 'E->A']
+        graph = Graph(vList, V)
         graph.replace(V['D->C'])
         newWeight = graph.goAcross(V['E->C'], V['D->C'][2])
         expectedNewWeight = V['E->C'][2]
-        self.assertEqual(graph.data['E'], V['E->C'])
-        self.assertEqual(graph.data['D'], V['D->E'])
-        self.assertEqual(graph.lastChange, V['E->B'])
+        self.assertEqual(graph.data[4], 'E->C')
+        self.assertEqual(graph.data[3], 'D->A')
+        self.assertEqual(graph.lastChange, 'E->A')
         self.assertEqual(newWeight, expectedNewWeight)
-        try:
-            graph.data['C']
-            self.assertTrue(False, "'C' is still a key")
-        except:
-            pass
     
     def test_a_graph_returns_none_when_constructed_with_duplicate_origin_vectors(self):
         V = self.V
         caughtError = False
         try:
-            Graph([V['D->E'], V['E->B'], V['B->D'], V['B->C']])
+            Graph(['A->B', 'B->D', 'B->C', 'D->A'], V)
         except BranchingGraphError:
             caughtError = True
         self.assertTrue(caughtError)
     
     def test_a_graph_can_check_for_difficult_incorrectness(self):
         V = self.V
-        vList = [V['A->B'], V['B->A'], V['C->E'], V['D->C'], V['E->D']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->A', 'C->E', 'D->C', 'E->D']
+        graph = Graph(vList, V)
         self.assertEqual(graph.isValid(), False)
     
     def test_a_can_output_a_list_of_its_vectors(self):
         V = self.V
-        vList = [V['A->B'], V['B->A'], V['C->E'], V['D->C'], V['E->D']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->A', 'C->E', 'D->C', 'E->D']
+        graph = Graph(vList, V)
         self.assertEqual(graph.toVectorListString(), "{<A->B:50><B->A:0><C->E:0><D->C:75><E->D:30>}")
     
     def test_a_graph_can_check_for_incorrectness(self):
         V = self.V
-        vList = [V['D->E'], V['E->B'], V['B->D'], V['C->B']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->D', 'C->B', 'D->A']
+        graph = Graph(vList, V)
         self.assertEqual(graph.isValid(), False)
     
     def test_a_graph_can_check_for_correctness(self):
         V = self.V
-        vList = [V['D->E'], V['E->B'], V['B->D']]
-        graph = Graph(vList)
+        vList = ['A->C', 'B->A', 'C->B']
+        graph = Graph(vList, V)
         self.assertEqual(graph.isValid(), True)
     
     def test_graph_has_a_weight(self):
         V = self.V
-        vList = [V['D->E'], V['E->B'], V['B->D']]
-        graph = Graph(vList)
-        self.assertEqual(graph.getWeight(), 230)
+        vList = ['A->B', 'B->C', 'C->A']
+        graph = Graph(vList, V)
+        self.assertEqual(graph.getWeight(), 375)
     
     def test_graph_can_output_a_sensible_string(self):
         V = self.V
-        vList = [V['D->E'], V['E->B'], V['B->D']]
-        graph = Graph(vList)
-        self.assertEqual(str(graph), "(D->E->B->D): 230")
+        vList = ['A->B', 'B->C', 'C->A']
+        graph = Graph(vList, V)
+        self.assertEqual(str(graph), "(A->B->C->A): 230")
     
     def test_graph_tracks_its_modifications(self):
         V = self.V
-        vList = [V['A->B'], V['B->C'], V['C->E'], V['D->A'], V['E->D']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->C', 'C->E', 'D->A', 'E->D']
+        graph = Graph(vList, V)
         self.assertEqual(None, graph.lastChange)
         graph.replace(V['D->C'])
-        self.assertEqual(str(V['D->A']), str(graph.lastChange))
+        self.assertEqual('D->A', str(graph.lastChange))
 
     def test_integrator_can_integrate_modifications_into_a_graph(self):
         V = self.V
-        vList = [V['A->B'], V['B->C'], V['C->E'], V['D->A'], V['E->D']]
-        newList = [V['A->B'], V['B->A'], V['C->E'], V['D->C'], V['E->D']]
-        graph = Graph(vList)
+        vList = ['A->B', 'B->C', 'C->E', 'D->A', 'E->D']
+        newList = ['A->B', 'B->A', 'C->E', 'D->C', 'E->D']
+        graph = Graph(vList, V)
         graph.replace(V['D->C'])
         graph.replace(V['B->D'])
         graph.replace(V['B->A'])
-        self.assertEqual(str(Graph(newList)), str(graph))
+        self.assertEqual(str(Graph(newList, V)), str(graph))
 
     def vectorCompare(self, v1, v2):
         self.assertEqual(str(v1), str(v2))
--- source/utilities/graph.py
+++ source/utilities/graph.py
@@ -7,55 +7,61 @@ class BranchingGraphError(Exception):
 
 class Graph(NoOpCompare):
 
-    def __init__(self, baseVectors, skipSpinUp=False):
+    def __init__(self, baseVectors, vectorList, skipSpinUp=False):
         if skipSpinUp:
             return
-        self.data = {}
+        self.data = []
         self.weight = 0
         self.lastChange = None
-        self.graphLength = len(baseVectors)
-        for vector in baseVectors:
-            self.__assignVector(vector)
+        self.vectorList = vectorList
+        for v in baseVectors:
+            vector = vectorList[v]
+            self.data.append(vector[0] + '->' + vector[1])
             self.weight += vector[2]
         if len(self.data) != len(baseVectors):
             raise BranchingGraphError
 
+    def translate(self, key):
+        return ord(key) - 65
+
     def copy(self):
-        g = Graph([], skipSpinUp=True)
-        g.data = copy.deepcopy(self.data)
+        g = Graph([], [], skipSpinUp=True)
+        g.data = list(self.data)
         g.weight = self.weight
         g.lastChange = self.lastChange
-        g.graphLength = self.graphLength
+        g.vectorList = self.vectorList
         return g
     
     def goAcross(self, vector, weight):
-        oldWeight = self.data[self.lastChange[0]][2]
+        oldWeight = self.vectorList[self.lastChange][2]
         self.swapOutLastChange()
         self.replace(vector)
         return  weight - oldWeight + vector[2]
     
     def swapOutLastChange(self):
-        self.replace(self.lastChange)
+        self.replace(self.vectorList[self.lastChange])
 
     def goDeeper(self, vector, weight):
         self.replace(vector)
         return weight + vector[2]
 
     def replace(self, vector):
-        v = self.data[vector[0]]
+        i = self.translate(vector[0])
+        v = self.data[i]
         self.lastChange = v
-        self.weight += vector[2] - v[2]
+        self.weight += vector[2] - self.vectorList[v][2]
         self.__assignVector(vector)
 
     def isValid(self):
         visited = {}
-        start = next(iter(self.data))[0]
+        start = self.data[0][0]
         beginning = start
         while True:
             if visited.get(start, False):
                 break
             visited[start] = True
-            start = self.data[start][1]
+            i = self.translate(start)
+            start = self.data[i][3]
         return len(visited) == len(self.data) and start == beginning
 
     def getWeight(self):
@@ -63,23 +69,25 @@ class Graph(NoOpCompare):
 
     def toVectorListString(self):
         s = "{"
-        for v in iter(self.data):
-            s += str(self.data[v])
+        for v in self.data:
+            s += str(self.vectorList[v])
         return s + "}"
 
     def __assignVector(self, vector):
-        self.data[vector[0]] = vector
+        i = self.translate(vector[0])
+        self.data[i] = vector[0] + '->' + vector[1]
 
     def __str__(self):
         visited = {}
-        start = next(iter(self.data))
+        start = self.data[0][0]
         literal = "(" + start
         while True:
-            if visited.get(start[0], False):
+            if visited.get(start, False):
                 break
             literal += "->"
-            visited[start[0]] = True
-            start = self.data[start][1]
+            visited[start] = True
+            i = self.translate(start)
+            start = self.data[i][3]
             literal += start
         return literal + "): " + str(self.weight)
 
